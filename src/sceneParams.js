8011640a193204fbc3f4a231d5a575acfd0df0f8
stmaccarelli <reddo.theking@gmail.com>
2016-09-24 17:40:44 +0200

    add sceneParams
    this structure will keep scenes settings.
    TODO: use this in sceneManager.js

--- src/sceneParams.js
+++ src/sceneParams.js
@@ -1,14 +1,222 @@
-//raibowdark
-{
-    "tiles": 268,
-    "repeatUV": 8.090002933527465,
-    "bFactor": 0.6875942559401627,
-    "cFactor": 0.013916406159565419,
-    "buildFreq": 81.46100634271541,
-    "natural": 0.1884444463154662,
-    "rainbow": 0.6364309276708011,
-    "squareness": 0.10534527021420541,
-    "map": "pattern1",
-    "landRGB": 16777215,
-    "horizonRGB": 0
+ HLS['scenesParams'] = {
+   // TODO:
+   // aggiungere
+  //  buildFreq
+  //  modelsGroup - quale gruppo spara quella scena
+  //  speedMultiplier
+
+  mizu:{
+      "tiles": 32,
+      "repeatUV": 512,
+      "bFactor": 0.22087281339397613,
+      "cFactor": 0.01543734109853323,
+      "buildFreq": 50.332836415087655,
+      "natural": 0.09748154523558328,
+      "rainbow": 0.8787699998982024,
+      "squareness": 0.22450016948639295,
+      "map": "land1 qw1q",
+      "landRGB": 1966335,
+      "horizonRGB": 0
+  },
+
+  solar_valley:{
+      "tiles": 304,
+      "repeatUV": 1,
+      "bFactor": 0.7700514086699899,
+      "cFactor": 0.22943490641847095,
+      "buildFreq": 83.1263442721574,
+      "natural": 0.09413511005639519,
+      "rainbow": 0.17452834968786823,
+      "squareness": 0.09949343069864669,
+      "map": "land4121qq",
+      "landRGB": 16777215,
+      "horizonRGB": 10631549
+  },
+
+  escher_surfers:{
+      "tiles": 62,
+      "repeatUV": 36.694396330169866,
+      "bFactor": 0.30674800052742746,
+      "cFactor": 0.2974944757881639,
+      "buildFreq": 69.0574646095875,
+      "natural": 0.2791990776048341,
+      "rainbow": 0.29879206695838567,
+      "squareness": 0.030770148836368028,
+      "map": "pattern1",
+      "landRGB": 16727705,
+      "horizonRGB": 7935
+  },
+
+
+  currybox:{
+      "tiles": 301,
+      "repeatUV": 1,
+      "bFactor": 0.22362124000822758,
+      "cFactor": 0.00048738724615287053,
+      "buildFreq": 36.395650378084945,
+      "natural": 0.052009118589931624,
+      "rainbow": 0.47001513975802833,
+      "squareness": 0.06006691434482253,
+      "map": "land2 ",
+      "landRGB": 16720418,
+      "horizonRGB": 30007
+  },
+
+  galaxy_glacier:{
+      "tiles": 160,
+      "repeatUV": 1,
+      "bFactor": 0.287989180087759,
+      "cFactor": 0.2148319562024518,
+      "buildFreq": 61.5837970429,
+      "natural": 0.7861551769529205,
+      "rainbow": 0.029628324585666777,
+      "squareness": 0.07198280149135716,
+      "map": "pattern5",
+      "landRGB": 11187452,
+      "horizonRGB": 6705
+  },
+
+  //drift movimento corsa più lento
+  //buildFreq su HLR.fft1
+  drift:{
+      "tiles": 475,
+      "repeatUV": 123,
+      "bFactor": 0.05129059413652515,
+      "cFactor": 0.2978844368180777,
+      "buildFreq": 15.403568342838847,
+      "natural": 0.8472923871307527,
+      "rainbow": 0.6627174494933723,
+      "squareness": 0.10965614357532677,
+      "map": "land2",
+      "landRGB": 16777215,
+      "horizonRGB": 6039170
+  },
+
+  // scorrimento veloce
+  // cielo con meno lampi
+  // buildFreq: fft2
+  // aerei
+
+
+
+  hyperocean: {
+      "tiles": 482,
+      "repeatUV": 7,
+      "bFactor": 0.8731022437271947,
+      "cFactor": 0.2509935600264457,
+      "buildFreq": 38.01100739053207,
+      "natural": 0.20053590285077338,
+      "rainbow": 0.08623985480612828,
+      "squareness": 0.16047922478789617,
+      "map": "land5",
+      "landRGB": 16369575,
+      "horizonRGB": 7173242
+  },
+
+  // balene
+
+  twin_horizon:{
+      "tiles": 99,
+      "repeatUV": 1,
+      "bFactor": 0.20445411338494512,
+      "cFactor": 0.0396025824963953,
+      "buildFreq": 45.50809304437684,
+      "natural": 0.8564485932135484,
+      "rainbow": 0.46009328606439337,
+      "squareness": 0.13619887944460984,
+      "map": "land2 ",
+      "landRGB": "#000fff",
+      "horizonRGB": 16728899
+  },
+
+  // da un certo punto random colors (quando il pezzo aumenta)
+  // da stesso punto aumenta velocità
+  else: {
+     "tiles": 104,
+      "repeatUV": 234,
+      "bFactor": 0.09684365733091505,
+      "cFactor": 0.10283514025561728,
+      "buildFreq": 30.804098302357595,
+      "natural": 0.9777840561709565,
+      "rainbow": 0.06458797021572349,
+      "squareness": 0.1739806389318333,
+      "map": "land",
+      "landRGB": 65399,
+      "horizonRGB": "#000000"
+  },
+
+  // lampi più intensi (il cielo ha colore base nero)
+  roger_water:{
+      "tiles": 216,
+      "repeatUV": 23,
+      "bFactor": 0.09206352399562667,
+      "cFactor": 0.09906018561143597,
+      "buildFreq": 58.39454669165698,
+      "natural": 0.33990308736861174,
+      "rainbow": 0.2435715269514518,
+      "squareness": 0.06904489162825383,
+      "map": "land5",
+      "landRGB": 5395026,
+      "horizonRGB": 519
+  },
+
+  popeye:{
+      "tiles": 293,
+      "repeatUV": 54.349171736919395,
+      "bFactor": 0.8743549159244348,
+      "cFactor": 0.2755886532412846,
+      "buildFreq": 95.57195980997615,
+      "natural": 0.45637631140860924,
+      "rainbow": 0.896952206289563,
+      "squareness": 0.00683205327040215,
+      "map": "land2",
+      "landRGB": 16288161,
+      "horizonRGB": 4334933
+  },
+
+
+  //extras
+  extra1:{
+      "tiles": 192,
+      "repeatUV": 1,
+      "bFactor": 0.693165412563634,
+      "cFactor": 0.19953982057571704,
+      "buildFreq": 10.113094716996041,
+      "natural": 0.3039475909118414,
+      "rainbow": 0.39132778953353453,
+      "squareness": 0.1737942804281068,
+      "map": "pattern4",
+      "landRGB": 16238264,
+      "horizonRGB": 4472153
+  },
+
+  //extrarainbow
+  extra2:{
+      "tiles": 208,
+      "repeatUV": 1,
+      "bFactor": 0.693165412563634,
+      "cFactor": 0.19953982057571704,
+      "buildFreq": 10.113094716996041,
+      "natural": 0.3039475909118414,
+      "rainbow": 0.39132778953353453,
+      "squareness": 0.1737942804281068,
+      "map": "pattern1",
+      "landRGB": 16777215,
+      "horizonRGB": 0
+  },
+
+  //extra3
+  extra3:{
+      "tiles": 268,
+      "repeatUV": 8.090002933527465,
+      "bFactor": 0.6875942559401627,
+      "cFactor": 0.013916406159565419,
+      "buildFreq": 81.46100634271541,
+      "natural": 0.1884444463154662,
+      "rainbow": 0.6364309276708011,
+      "squareness": 0.10534527021420541,
+      "map": "pattern1",
+      "landRGB": 16777215,
+      "horizonRGB": 0
+  }
 }
